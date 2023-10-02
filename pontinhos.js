3d6746e509ed82838689267fd6aabd4b371a2c88
Luizhenrique3873 <112209717+Luizhenrique3873@users.noreply.github.com>
2023-10-01 22:05:23 -0300

    Update pontinhos.js

--- pontinhos.js
+++ pontinhos.js
@@ -7,3 +7,22 @@ function ir() {
     document.getElementById("vai").innerHTML = "o cpf não foi encontrado";
     }
 }
+var cpfInput = document.getElementById('cpfid');
+
+    cpfInput.addEventListener('input', function (e) {
+        var inputValue = e.target.value.replace(/\D/g, ''); // Remove todos os caracteres não numéricos
+        var formattedValue = formatCPF(inputValue);
+        e.target.value = formattedValue;
+    });
+
+    function formatCPF(value) {
+        if (value.length <= 3) {
+            return value;
+        } else if (value.length <= 6) {
+            return value.substring(0, 3) + '.' + value.substring(3);
+        } else if (value.length <= 9) {
+            return value.substring(0, 3) + '.' + value.substring(3, 6) + '.' + value.substring(6);
+        } else {
+            return value.substring(0, 3) + '.' + value.substring(3, 6) + '.' + value.substring(6, 9) + '-' + value.substring(9);
+        }
+    }
