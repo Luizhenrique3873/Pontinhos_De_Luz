72fa02d75db0846ec8bc2c6211a1d5254f280859
Luizhenrique3873 <112209717+Luizhenrique3873@users.noreply.github.com>
2023-12-10 19:02:01 -0300

    Add files via upload

--- pontinhos.js
+++ pontinhos.js
@@ -1,10 +1,16 @@
 function ir() {
     var cpfvar = document.getElementById("cpfid").value; //quando var esta fora de function as vezes não funciona
     if (cpfvar == "41193446813"){
-    window.location.href = "heitor.html";
+        window.location.href = "heitor.html";
     }
     if (cpfvar == "44741454886" || cpfvar == "44303201871"){
-    window.location.href = "rhavi.html";
+        window.location.href = "rhavi.html";
+    }
+    if (cpfvar == "05606644355"){
+        window.location.href = "lucca.html";
+    }
+    if (cpfvar == "36021457862"){
+        window.location.href = "luan.html";
     }
     else {
     document.getElementById("vai").innerHTML = "O CPF Não Foi Encontrado";
